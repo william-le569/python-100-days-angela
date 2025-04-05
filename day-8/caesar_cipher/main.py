005769a6da1743b728a407f96745b58038e0f5c9
Bill <thieu.le569@gmail.com>
2025-04-06 01:09:38 +0700

    add decrypt function to caesar cipher project

--- day-8/caesar_cipher/main.py
+++ day-8/caesar_cipher/main.py
@@ -11,12 +11,30 @@ def encrypt(original_text, shift_amount):
     original_text_list = list(original_text)
     encrypt_text_list = []
     for char in original_text_list:
-        encrypt_text_list.append(chr(ord('a') + (ord(char) + shift_amount- ord('a'))%26))
+        if ord(char) >= ord("a") and ord(char) <= ord("z"): 
+            encrypt_text_list.append(chr(ord('a') + (ord(char) + shift_amount- ord('a'))%26))
+        else:
+            encrypt_text_list.append(char)
     encrypt_text = "".join(encrypt_text_list)
     return encrypt_text
 
-encrypt_text = encrypt(text, shift)
-print(encrypt_text)
+def decrypt(encrypted_text, shift_amount):
+    encrypt_text_list = list(encrypted_text)
+    decrypt_text_list = []
+    for char in encrypt_text_list:
+        if ord(char) >= ord("a") and ord(char) <= ord("z"): 
+            decrypt_text_list.append(chr(ord('a') + (ord(char) - shift_amount- ord('a'))%26))
+        else:
+            decrypt_text_list.append(char)
+    decrypt_text = "".join(decrypt_text_list)
+    return decrypt_text
+
+if direction == "encode":
+    encrypt_text = encrypt(text, shift)
+    print(encrypt_text)
+elif direction == "decode":
+    decrypt_text = decrypt(text, shift)
+    print(decrypt_text)
 
 
 #TODO-2: Inside encrypt -> shift original text by shift amount
