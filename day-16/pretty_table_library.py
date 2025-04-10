aea67f61c690fed5ff313ee7866e218e5b4494ed
Bill <thieu.le569@gmail.com>
2025-04-10 23:48:53 +0700

    day - 16: finish oop_coffee_machine

--- day-16/pretty_table_library.py
+++ day-16/pretty_table_library.py
@@ -1,2 +1,11 @@
-import prettytable
-x = prettytable.PrettyTable()
+# import prettytable
+# x = prettytable.PrettyTable()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("pokemon name".title(), ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("type".title(), ["Electric", "Water", "Fire"])
+table.align = "l"
+
+print(table)
